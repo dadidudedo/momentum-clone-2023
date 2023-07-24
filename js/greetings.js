faafd795fd3754a8933a7b7e52f6a65a75798ad9
daeun Kong <daeun113@ajou.ac.kr>
2023-07-24 11:12:36 +0900

    complete clock.

--- js/greetings.js
+++ js/greetings.js
@@ -5,8 +5,6 @@ const greeting = document.querySelector("#greeting");
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
 
-const savedUsername = localStorage.getItem(USERNAME_KEY);
-
 function onLoginSubmit(event) {
   event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
@@ -20,6 +18,7 @@ function paintGreetings(username) {
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
+const savedUsername = localStorage.getItem(USERNAME_KEY);
 if (savedUsername === null) {
   loginForm.classList.remove(HIDDEN_CLASSNAME);
   loginForm.addEventListener("submit", onLoginSubmit);
